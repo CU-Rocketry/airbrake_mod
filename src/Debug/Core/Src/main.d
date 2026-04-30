a05cbdd1006a24ccdda0af7190c890371529e249
Sigmond Kukla <sigkukla@gmail.com>
2026-04-30 12:53:30 -0400

    Integrate changes with main

--- src/Debug/Core/Src/main.d
+++ src/Debug/Core/Src/main.d
@@ -45,8 +45,9 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Core/Inc/state.h ../Core/Inc/mode_switch.h \
  ../Core/Inc/state_estimation.h ../Drivers/CMSIS/DSP/Include/arm_math.h \
  ../Drivers/CMSIS/Include/cmsis_compiler.h ../Core/Inc/state.h \
- ../Core/Inc/batt_sense.h ../Core/Inc/telemetry.h ../Core/Inc/flash.h \
- ../Core/Inc/control.h
+ ../Core/Inc/batt_sense.h ../Core/Inc/telemetry.h ../Core/Inc/packets.h \
+ ../Core/Inc/cobs_uart.h ../Core/Inc/flash.h ../Core/Inc/control.h \
+ ../Core/Inc/packets.h ../Core/Inc/hil.h
 ../Core/Inc/main.h:
 ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal.h:
 ../Core/Inc/stm32h7xx_hal_conf.h:
@@ -103,5 +104,9 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Core/Inc/state.h:
 ../Core/Inc/batt_sense.h:
 ../Core/Inc/telemetry.h:
+../Core/Inc/packets.h:
+../Core/Inc/cobs_uart.h:
 ../Core/Inc/flash.h:
 ../Core/Inc/control.h:
+../Core/Inc/packets.h:
+../Core/Inc/hil.h:
