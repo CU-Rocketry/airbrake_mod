fa119ee06ae1896320e4532799c3799764618e2e
Sigmond Kukla <sigkukla@gmail.com>
2026-05-02 01:36:43 -0400

    Reduce control system target apogee for testbed

--- src/Core/Inc/control.h
+++ src/Core/Inc/control.h
@@ -13,7 +13,9 @@
 #define ROCKET_MASS_EMPTY 24.526f   // [kg]
 #define ROCKET_CD 0.32f // Tunable drag coefficient
 #define ROCKET_A_REF 0.013478f // [m^2] Reference area
-#define TARGET_APOGEE 3048.0f // [m] 10,000 ft
+// #define TARGET_APOGEE 3048.0f // [m] 10,000 ft
+#define TARGET_APOGEE 850.0f // [m] for testbed
+
 
 void control_update(float dt);
 float predict_apogee(float alt_agl, float vel_z, float g, float Cd, float a_ref, float mass_empty);
