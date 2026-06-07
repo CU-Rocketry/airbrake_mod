b15aa640fdae214ca739666b3c4e40c69e725052
Sigmond Kukla <sigkukla@gmail.com>
2026-06-07 16:55:52 -0400

    Add mask to togglable state flag

--- python/controlpanel/gui/components/flags.py
+++ python/controlpanel/gui/components/flags.py
@@ -1,8 +1,7 @@
 from imgui_bundle import imgui
 from backend.firmware_schema import ffi
 
-STATE_FLAGS = ffi.typeof("state_flags_t").relements
-STATE_FLAGS = {}
+STATE_FLAGS = ffi.typeof("state_flag_t").relements
 
 def draw_state_flag_readonly(label: str, current_flags: int, flag_name: str):
     mask = STATE_FLAGS.get(flag_name, 0) # get bitmask for requested flag
@@ -17,14 +16,14 @@ def draw_state_flag_readonly(label: str, current_flags: int, flag_name: str):
     imgui.checkbox(label, is_set)
     imgui.end_disabled()
 
-def draw_state_flag_toggle(label: str, current_flags: int, flag_name: str) -> tuple[bool, bool]:
+def draw_state_flag_toggle(label: str, current_flags: int, flag_name: str) -> tuple[bool, bool, int]:
     mask = STATE_FLAGS.get(flag_name, 0) # get bitmask for requested flag
 
     if mask == 0: # if flag not defined, show error
         imgui.text_colored((1.0, 0.0, 0.0, 1.0), f"Flag not found: {label}")
-        return False, False
+        return False, False, 0 # no change, value false, zero mask
 
     is_set = bool(current_flags & mask) # determine if flag is currently set
     
     changed, value = imgui.checkbox(label, is_set) # toggleable checkbox
-    return changed, value # return whether changed and the new value
+    return changed, value, mask # return whether changed, the new value, and the mask for this flag
