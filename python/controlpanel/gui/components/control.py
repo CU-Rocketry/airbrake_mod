5f78eeb4de489d05528547b44c115120df4c1ded
Sigmond Kukla <sigkukla@gmail.com>
2026-06-13 15:22:21 -0400

    Add control system state flags

--- python/controlpanel/gui/components/control.py
+++ python/controlpanel/gui/components/control.py
@@ -4,6 +4,9 @@ from gui.components.plots import LinePlot
 from gui.components.flags import draw_state_flag_toggle, draw_state_flag_readonly, STATE_FLAGS
 from backend.telemetry import send_command
 
+plot_predicted = LinePlot("Predicted Apogee", "Prediction [m]")
+plot_predicted.add_line("Predicted", app_state.buffers['predicted'])
+
 plot_output = LinePlot("Output", "Deployment command [0-1]")
 plot_output.add_line("Output", app_state.buffers['output'])
 
@@ -13,6 +16,15 @@ plot_contrib.add_line("I", app_state.buffers['i_contrib'])
 
 def draw_control():
     imgui.begin("Control")
+    imgui.text("Deployment Lockout Conditions")
+    draw_state_flag_readonly("Lockout Accel", app_state.current_flags, "FLAG_LOCKOUT_ACCEL")
+    draw_state_flag_readonly("Lockout Altitude", app_state.current_flags, "FLAG_LOCKOUT_ALTITUDE")
+    draw_state_flag_readonly("Lockout Elapsed", app_state.current_flags, "FLAG_LOCKOUT_ELAPSED")
+    draw_state_flag_readonly("Lockout Apogee", app_state.current_flags, "FLAG_LOCKOUT_APOGEE")
+    draw_state_flag_readonly("Lockout Attitude", app_state.current_flags, "FLAG_LOCKOUT_ATTITUDE")
+    imgui.separator()
+    plot_predicted.render(app_state.latest_time)
+    imgui.separator()
     plot_output.render(app_state.latest_time)
     plot_contrib.render(app_state.latest_time)
     imgui.end()
